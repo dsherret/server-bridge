548bcf9e96ec32463bcc607387dddfe9686c7d60
David Sherret <dsherret@users.noreply.github.com>
2016-06-27 21:28:23 -0400

    Delete routes.ts

--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-import {RouteDefinition} from "./RouteDefinition";
-
-export class Routes {
-    // do not put a default values on these properties because it will overwrite the prototype value
-    routeDefinitions: RouteDefinition[];
-    basePath: string;
-}
